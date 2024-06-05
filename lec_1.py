1ef329da2310b04c497a3cf8a4d2d65d4f00bef6
shubh935 <sk9216773@gmail.com>
2024-06-05 09:06:58 +0530

    my pytjon code

---
+++ lec_1.py
@@ -0,0 +1,9 @@
+ #print("gello",sep ="=")
+# print("hello")
+
+x = 5
+y = 4
+
+print(x,y,sep = "_",end = "$") # sep is used to give space/extra kuch v between variable
+                              #end is bydefault in print function we can also change the end value
+print("  hello")
